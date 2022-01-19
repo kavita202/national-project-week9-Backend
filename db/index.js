4ff6b2988fcfa69dcd9a539ff43d7f296e0a978b
Lorcan Mc <lorcan.jmc@gmail.com>
2022-01-19 15:28:33 +0000

    database url added

--- db/index.js
+++ db/index.js
@@ -3,6 +3,7 @@ import * as config from '../config.js'
 
 const pool = new pg.Pool({
   //Heroku credentials are now environment variables
+  connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false},
   });
 
