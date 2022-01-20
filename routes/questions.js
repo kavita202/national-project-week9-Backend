c94fe3bca8dfddb0c10e190155934cc2246d9681
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-01-20 11:01:57 +0000

    testing1

--- routes/questions.js
+++ routes/questions.js
@@ -4,22 +4,22 @@ import { getQuestionsByTopic, getAllQuestions } from "../models/questions.js";
 
 /* GET users listing. */
 router.get("/questions", async function (req, res) {
-  // let topicname = req.query.topic;
-  // console.log(topicname);
-  // if (topicname) {
-  //   const data = await getQuestionsByTopic(topicname);
-  //   res.json({
-  //     success: true,
-  //     message: `Found questions by ${topicname}`,
-  //     payload: data,
-  //   });
-  //   return;
-  // }
-  // const allquestions = await getAllQuestions();
+  let topicname = req.query.topic;
+  console.log(topicname);
+  if (topicname) {
+    const data = await getQuestionsByTopic(topicname);
+    res.json({
+      success: true,
+      message: `Found questions by ${topicname}`,
+      payload: data,
+    });
+    return;
+  }
+  const allquestions = await getAllQuestions();
   res.json({
     success: true,
     message: "here are all the questions",
-    payload:" allquestions",
+    payload:allquestions,
   });
 });
 
