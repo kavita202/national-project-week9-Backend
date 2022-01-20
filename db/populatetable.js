52129730f7b7c65ee74462bd0c24fee0b8f487d4
unknown <kavita.kaur998@gmail.com>
2022-01-20 15:45:45 +0000

    javascript questions added

--- db/populatetable.js
+++ db/populatetable.js
@@ -1,17 +1,19 @@
-import query from './index.js'
-import data from '../data.js'
-console.log(data[0].Incorrect_answers)
+import query from "./index.js";
+import data2 from "../data2.js";
+console.log(data2[0].Incorrect_answers);
 async function populateQuestionsTable() {
-    for (let i = 0; i < data.length; i++) {
-        const Topic = data[i].Topic
-        const Question = data[i].Question
-        const Correct_answer = data[i].Correct_answer
-        const Incorrect_answers = data[i].Incorrect_answers
+  for (let i = 0; i < data2.length; i++) {
+    const Topic = data2[i].Topic;
+    const Question = data2[i].Question;
+    const Correct_answer = data2[i].Correct_answer;
+    const Incorrect_answers = data2[i].Incorrect_answers;
 
-        const res = await query(`INSERT INTO questions (topic, question, correct_answer, incorrect_answers) VALUES ($1, $2, $3, $4) RETURNING *;`,
-            [Topic, Question, Correct_answer, Incorrect_answers])
-        console.log(res)
-    }
-};
+    const res = await query(
+      `INSERT INTO questions (topic, question, correct_answer, incorrect_answers) VALUES ($1, $2, $3, $4) RETURNING *;`,
+      [Topic, Question, Correct_answer, Incorrect_answers]
+    );
+    console.log(res);
+  }
+}
 
 populateQuestionsTable();
