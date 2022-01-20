df7db4496f48f6144b5231ef14247c4c649b8fcb
Lorcan Mc <lorcan.jmc@gmail.com>
2022-01-20 09:20:17 +0000

    cors allow all access

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ const app = express();
 // }
 
 app.use(logger('dev'));
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
