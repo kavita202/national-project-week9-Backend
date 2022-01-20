cff97655a74e5214a541ac919b0f4c0fbd3c4331
unknown <kavita.kaur998@gmail.com>
2022-01-20 16:13:03 +0000

    resources table made and function to add in resourcedata

--- db/createResourcesTable.js
+++ db/createResourcesTable.js
@@ -1,14 +1,15 @@
-import query from './index.js'
+import query from "./index.js";
 
 const sqlString = `CREATE TABLE IF NOT EXISTS resources
 (id SERIAL PRIMARY KEY,
 topic TEXT,
-resources TEXT[]
-);`
+title TEXT,
+url TEXT
+);`;
 
-async function createQuestionsTable() {
-    const res = await query(sqlString)
-    console.log('Resources table created:', res)
+async function createResourcesTable() {
+  const res = await query(sqlString);
+  console.log("Resources table created:", res);
 }
 
-createQuestionsTable()
+createResourcesTable();
---
+++ db/populateResourceTable.js
@@ -0,0 +1,18 @@
+import query from "./index.js";
+import resourceData from "../resourcesData.js";
+
+async function populateResourcesTable() {
+  for (let i = 0; i < resourceData.length; i++) {
+    const topic = resourceData[i].topic;
+    const title = resourceData[i].title;
+    const url = resourceData[i].url;
+
+    const res = await query(
+      `INSERT INTO resources (topic, title, url) VALUES ($1, $2, $3, $4) RETURNING *;`,
+      [topic, title, url]
+    );
+    console.log(res);
+  }
+}
+
+populateResourcesTable();
