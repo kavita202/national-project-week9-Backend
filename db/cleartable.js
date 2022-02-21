d4ad635cc41a4e66dc1187e1ec249721a13e9e6f
unknown <kavita.kaur998@gmail.com>
2022-02-21 00:07:11 +0000

    renamed in package json

--- db/cleartable.js
+++ db/cleartable.js
@@ -1,7 +1,6 @@
 import query from "./index.js";
 
-const sqlString = `TRUNCATE TABLE scores;
-;`;
+const sqlString = `TRUNCATE TABLE scores;`;
 
 async function deleteScores() {
   const res = await query(sqlString);
